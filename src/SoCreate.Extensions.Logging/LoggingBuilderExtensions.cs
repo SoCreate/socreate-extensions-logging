c723a1294c2ba31a51d3848dcc7118ba96309bfb
Dana.Desrosiers <Dana.Desrosiers@SoCreate.it>
2019-06-05 12:17:44 -0700

    moved ApplicationInsights telemetry service registration into the LoggingBuilderExtensions

--- src/SoCreate.Extensions.Logging/LoggingBuilderExtensions.cs
+++ src/SoCreate.Extensions.Logging/LoggingBuilderExtensions.cs
@@ -2,11 +2,12 @@
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
-using Serilog.Extensions.Logging;
 using SoCreate.Extensions.Logging.ActivityLogger;
 using SoCreate.Extensions.Logging.ServiceFabric;
 using System;
 using System.Fabric;
+using System.Text.Encodings.Web;
+using SoCreate.Extensions.Logging.ApplicationInsightsLogger;
 
 namespace SoCreate.Extensions.Logging
 {
@@ -24,11 +25,18 @@ namespace SoCreate.Extensions.Logging
             }
 
             builder.Services.AddSingleton<LoggingLevelSwitch>();
-        
+
             builder.Services.AddTransient<Action<ServiceContext>>(serviceProvider => EnrichLoggerWithContext(serviceProvider));
             builder.Services.AddTransient<LoggerConfiguration>(services => GetLoggerConfiguration(services));
             builder.Services.AddTransient<ActivityLoggerLogConfigurationAdapter>();
-            builder.Services.AddTransient<ApplicationInsightsLoggerLogConfigurationAdapter>();
+
+            if (options.UseApplicationInsights)
+            {
+                builder.Services.AddApplicationInsightsTelemetry();
+                builder.Services.AddApplicationInsightsTelemetryProcessor<RemoveDuplicateExceptionLogsProcessor>();
+                builder.Services.AddTransient<ApplicationInsightsLoggerLogConfigurationAdapter>();
+                builder.Services.AddTransient(serviceProvider => JavaScriptEncoder.Default);
+            }
 
             builder.Services.AddSingleton<ILoggerProvider, LoggerProvider>(services => GetLoggerProvider(services, options));
             builder.AddFilter<LoggerProvider>(null, LogLevel.Trace);
@@ -56,7 +64,7 @@ namespace SoCreate.Extensions.Logging
                 serviceProvider.GetRequiredService<ActivityLoggerLogConfigurationAdapter>()
                     .ApplyConfiguration(loggerConfig);
             }
-            
+
             return new LoggerProvider(loggerConfig.CreateLogger());
         }
 
